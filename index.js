88ba6ec2c8c1384991145a307578081927ff45a0
yash mundhada <ymundhada12@gmail.com>
2022-09-28 14:18:10 +0530

    addn logic done

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(){
+    var num1 = document.getElementById("firstno").value
+    var num2 = document.getElementById("secondno").value
+
+    let result = num1+num2
+
+    document.getElementById("result").value = result
+
+
+}
